e53b2362cce63ec4a2e26f8427c2c03cf0bd3d01
zdzeni <jenni.petakoski@gmail.com>
2024-01-23 15:20:11 +0200

    Play metodin logiikka ja alkuaineet.txt lisäys

--- Program.cs
+++ Program.cs
@@ -33,7 +33,32 @@ namespace AlkuAineet
             }
             public static void play()
             {
+                //loopataan kunnes 5 vastausta on täynnä
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("Anna alkuaine");
+                    answer = Console.ReadLine().Trim().ToLower(); // Convert the input to lowercase
 
+                    //tarkistaa onko samaa vastausta käytetty aiemmin
+                    if (elements.Contains(answer))
+                    {
+                        Console.Write("Voit antaa saman alkuaineen vain kerran.");
+                    }
+                    else
+                    {
+                        // Lisää "answer" listalle "elements"
+                        elements.Add(answer);
+                    }
+                
+                    // Tarkista, onko alkuaine tiedostossa "alkuaineet.txt"
+                    if (File.ReadLines("alkuaineet.txt").Any(line => line.Equals(answer StringComparison.OrdinalIgnoreCase)))
+                    {
+                        // Lisää yksi piste "grade"
+                        grade++;
+                    }
+
+
+                }
             }
 
             public static void review()
