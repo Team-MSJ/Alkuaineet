4f00de74acdd64792ec4548476ad40fd0349aafd
Satu Rouvari <128889088+SatuSuopanki@users.noreply.github.com>
2024-01-24 10:17:25 +0200

    Muutettu valikon koodi metodiksi. Lisätty valikkokutsu metodien Play ja Review loppuun. Muutettu metodien Play ja Review nimet alkamaan isolla alkukirjaimella.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 namespace AlkuAineet
 {
     using System;
-    using System.IO 
-    using newtonsoft.json 
+    using System.IO
+    using newtonsoft.json
     public class Program
     {
 
@@ -14,26 +14,37 @@ namespace AlkuAineet
 
             List<string> answers = new List<string>();
 
-            Console.WriteLine("Valitse, haluatko");
-            Console.WriteLine("P = Pelata");
-            Console.WriteLine("T = Tarkastella tuloksia");
-            string input = Console.ReadLine();
-            if (input = "P")
+            public static void Valikko()
             {
-                play();
+                Console.WriteLine("Valitse, haluatko");
+                Console.WriteLine();
+                Console.WriteLine("  P = Pelata");
+                Console.WriteLine("  T = Tarkastella tuloksia");
+                Console.WriteLine("  L = Lopettaa");
+                string input = Console.ReadLine();
+                if (input = "P")
+                {
+                    Play();
+                }
+                else if (input = "T")
+                {
+                    Review();
+                }
+                else if (input = L)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Valitse uudelleen");
+                }
             }
-            else if (input = "T")
-            {
-                review();
-            }
-            else
-            {
-                Console.WriteLine("Valitse uudelleen");
-            }
+
+
             {
 
             }
-            public static void play()
+            public static void Play()
             {
                 //loopataan kunnes 5 vastausta on täynnä
                 for (int i = 0; i < 5; i++)
@@ -51,35 +62,39 @@ namespace AlkuAineet
                         // Lisää "answer" listalle "elements"
                         elements.Add(answer);
                     }
-                
+
                     // Tarkista, onko alkuaine tiedostossa "alkuaineet.txt"
                     if (File.ReadLines("alkuaineet.txt").Any(line => line.Equals(answer StringComparison.OrdinalIgnoreCase)))
                     {
                         // Lisää yksi piste "grade"
                         grade++;
                     }
-                Console.Write("Anna kuluva päivä muodossa PPKKVVVV: ");
-                string date = Console.ReadLine;
+                    Console.Write("Anna kuluva päivä muodossa PPKKVVVV: ");
+                    string date = Console.ReadLine;
 
-                bool doesDirectoryExist = Directory.Exists(date);
-                if (true)
-                {
-                    break;
+                    bool doesDirectoryExist = Directory.Exists(date);
+                    if (true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), date));
+                    }
+
+
+
                 }
-                else
-                {
-                    Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), date));
-                }
-
-
-
-                }
-
+                Valikko();
             }
 
-            public static void review()
+            public static void Review()
             {
 
+
+
+
+                Valikko();
             }
         }
     }
